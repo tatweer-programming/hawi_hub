f9eb60404782a6945fe4d0c405fadcf943ed9903
MAHMOUD ATEF <mahmoud.atef3167@gmail.com>
2024-03-17 02:04:52 +0200

    create app router

---
+++ lib/src/core/routing/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hawihub/src/core/routing/routes.dart';
+import 'package:hawihub/src/modules/main/view/screens/main_screen.dart';
+
+import '../../modules/main/view/screens/splash_screen.dart';
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splash:
+        return MaterialPageRoute(
+            builder: (_) => const SplashScreen(
+                  nextScreen: MainScreen(),
+                ));
+
+      default:
+        return MaterialPageRoute(
+            builder: (_) => const SplashScreen(
+                  nextScreen: MainScreen(),
+                ));
+    }
+  }
+}
---
+++ lib/src/core/routing/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String places = '/places';
+  static const String comments = '/comments';
+}
